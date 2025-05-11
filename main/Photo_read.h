8fcdf1bf5499ff8c3c406d44f3d5c7cf3896fc40
zzppss-universe <universe.ee12@nycu.edu.tw>
2025-05-11 20:53:16 +0800

    photo

--- main/Photo_read.h
+++ main/Photo_read.h
@@ -1,22 +1,25 @@
 #include "Global_define.h"
 // 初始化光敏電阻參數
-void setupPhoto(){
-  for(int i=0;i<4;i++){
-    threshold[i] = 0;
+void setupPhoto()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    threshold[i] = 400;
     CoinData[i] = 0;
     CoinActive[i] = 0;
   }
-
 }
 
 // 讀取光敏電阻，並設定平均值
-void ReadPhoto(){
+void ReadPhoto()
+{
   Serial.println("Current :");
-  for(int t=0;t<4;t++){
+  for (int t = 0; t < 4; t++)
+  {
     CoinData[t] = analogRead(photoPins[t]);
-    threshold[t] = 63*(threshold[t]/64.0) + ( CoinData[t] )/64.0;
-    
-    if(CoinData[t] > threshold[t]*hardThreshold)
+    threshold[t] = 63 * (threshold[t] / 64.0) + (CoinData[t]) / 64.0;
+
+    if (CoinData[t] > threshold[t] * hardThreshold)
       CoinActive[t] = 1;
     else
       CoinActive[t] = 0;
