96c96ceb1565e9b964f22350ada42108b812d1d9
zzppss-universe <universe.ee12@nycu.edu.tw>
2025-05-17 18:33:30 +0800

    test

--- main/SPI_communication.h
+++ main/SPI_communication.h
@@ -16,7 +16,7 @@ void sendCommand() {
     else if (button_currentValue[2] == 1) command_current =SPI.transfer(REC);
     else if (button_currentValue[3] == 1) command_current =SPI.transfer(ERASE);
     else if (button_currentValue[4] == 1) command_current =SPI.transfer(FWD);
-    else command_current = SPI.transfer(RESET);
+    else if (button_currentValue[5] == 1) command_current = SPI.transfer(RESET);
     delay(10);  // 確保指令傳輸完成
     digitalWrite(SS_Pin, HIGH);
     SPI.endTransaction();  
