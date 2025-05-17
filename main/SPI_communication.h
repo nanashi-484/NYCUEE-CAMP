9bb2706bb69b4bb958f8b2185d13f32b292ae226
zzppss-universe <universe.ee12@nycu.edu.tw>
2025-05-17 18:42:00 +0800

    eset

--- main/SPI_communication.h
+++ main/SPI_communication.h
@@ -9,13 +9,14 @@ void setupSPI() {
 
 
 void sendCommand() {
-    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    SPI.beginTransaction(SPISettings(500000, LSBFIRST, SPI_MODE3));
     digitalWrite(SS_Pin, LOW);
-    if (button_currentValue[0] == 1) command_current =SPI.transfer(PLAY);
-    else if (button_currentValue[1] == 1) command_current =SPI.transfer(STOP);
-    else if (button_currentValue[2] == 1) command_current =SPI.transfer(REC);
-    else if (button_currentValue[3] == 1) command_current =SPI.transfer(ERASE);
-    else if (button_currentValue[4] == 1) command_current =SPI.transfer(FWD);
+    Serial.print("sac");
+    if (button_currentValue[0] == 1) command_current = SPI.transfer(PLAY);
+    else if (button_currentValue[1] == 1) command_current = SPI.transfer(STOP);
+    else if (button_currentValue[2] == 1) command_current = SPI.transfer(REC);
+    else if (button_currentValue[3] == 1) command_current = SPI.transfer(ERASE);
+    else if (button_currentValue[4] == 1) command_current = SPI.transfer(FWD);
     else if (button_currentValue[5] == 1) command_current = SPI.transfer(RESET);
     delay(10);  // 確保指令傳輸完成
     digitalWrite(SS_Pin, HIGH);
